93f9b3cda25b292e065db4e8395aedfc2191bd55
Tiago Silva <tiagosik1994@gmail.com>
2021-03-23 21:20:35 +0000

    feat: add db.proprieties and getDataSource method
    This was implemented so it's possible to create a DataSource from a config file.

---
+++ src/main/java/pt/isel/ls/utils/Utils.java
@@ -0,0 +1,28 @@
+package pt.isel.ls.utils;
+
+import org.postgresql.ds.PGSimpleDataSource;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class Utils {
+
+    public static PGSimpleDataSource getDataSource(){
+        Properties configs = new Properties();
+        FileInputStream config_file = null;
+        PGSimpleDataSource postgres = null;
+        try {
+            config_file = new FileInputStream("db.properties");
+            configs.load(config_file);
+            postgres = new PGSimpleDataSource();
+            postgres.setURL(configs.getProperty("PSQL_DB_URL"));
+            postgres.setUser(configs.getProperty("PSQL_DB_USERNAME"));
+            postgres.setPassword(configs.getProperty("PSQL_DB_PASSWORD"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return postgres;
+    }
+
+}
