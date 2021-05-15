1610e83538d48d7539b8b3da7a2264710b05c714
Tiago Silva <tiagosik1994@gmail.com>
2021-05-15 23:27:10 +0100

    feat: change PGSimpleDataSource to get env variables

--- src/main/java/pt/isel/ls/utils/Utils.java
+++ src/main/java/pt/isel/ls/utils/Utils.java
@@ -1,27 +1,16 @@
 package pt.isel.ls.utils;
 
 import org.postgresql.ds.PGSimpleDataSource;
+import static java.lang.System.getenv;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.Properties;
 
 public class Utils {
 
     public static PGSimpleDataSource getDataSource() {
-        Properties configs = new Properties();
-        FileInputStream configFile = null;
-        PGSimpleDataSource postgres = null;
-        try {
-            configFile = new FileInputStream("db.properties");
-            configs.load(configFile);
-            postgres = new PGSimpleDataSource();
-            postgres.setURL(configs.getProperty("PSQL_DB_URL"));
-            postgres.setUser(configs.getProperty("PSQL_DB_USERNAME"));
-            postgres.setPassword(configs.getProperty("PSQL_DB_PASSWORD"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        PGSimpleDataSource postgres = new PGSimpleDataSource();
+        postgres.setURL(getenv("PSQL_DB_URL"));
+        postgres.setUser(getenv("PSQL_DB_USERNAME"));
+        postgres.setPassword(getenv("PSQL_DB_PASSWORD"));
         return postgres;
     }
 
