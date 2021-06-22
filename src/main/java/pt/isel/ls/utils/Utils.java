9470b00664215f86f6953d3d319b064164e7893f
Tiago Silva <tiagosik1994@gmail.com>
2021-06-22 19:21:59 +0100

    fix: undo the last commit on utils

--- src/main/java/pt/isel/ls/utils/Utils.java
+++ src/main/java/pt/isel/ls/utils/Utils.java
@@ -11,22 +11,8 @@ import java.util.Properties;
 public class Utils {
 
     public static PGSimpleDataSource getDataSource() {
-        //PGSimpleDataSource postgres = new PGSimpleDataSource();
-        //postgres.setURL(getenv("JDBC_DATABASE_URL"));
-        //return postgres;
-        Properties configs = new Properties();
-        FileInputStream configFile = null;
-        PGSimpleDataSource postgres = null;
-        try {
-            configFile = new FileInputStream("db.propertiesLA");
-            configs.load(configFile);
-            postgres = new PGSimpleDataSource();
-            postgres.setURL(configs.getProperty("PSQL_DB_URL"));
-            postgres.setUser(configs.getProperty("PSQL_DB_USERNAME"));
-            postgres.setPassword(configs.getProperty("PSQL_DB_PASSWORD"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        PGSimpleDataSource postgres = new PGSimpleDataSource();
+        postgres.setURL(getenv("JDBC_DATABASE_URL"));
         return postgres;
     }
 }
